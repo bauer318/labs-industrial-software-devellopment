021a97167ce0a0da80d1fbe0f3e2d717cd57a0b1
Jacques kibamba <jacqueskibamba@gmail.com>
2022-09-24 09:13:46 +0300

    updated Home controller

--- LW1/src/main/java/ru/rsreu/kibamba/lw1/HomeController.java
+++ LW1/src/main/java/ru/rsreu/kibamba/lw1/HomeController.java
@@ -1,6 +1,5 @@
 package ru.rsreu.kibamba.lw1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class HomeController {
