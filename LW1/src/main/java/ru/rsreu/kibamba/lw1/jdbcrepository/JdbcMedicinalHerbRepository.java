e45eb6df4187b8c3def7109415d5f7beb0d4dfce
Jacques kibamba <jacqueskibamba@gmail.com>
2022-10-07 23:36:01 +0300

    used SimpleJdbcInsert

--- LW1/src/main/java/ru/rsreu/kibamba/lw1/jdbcrepository/JdbcMedicinalHerbRepository.java
+++ LW1/src/main/java/ru/rsreu/kibamba/lw1/jdbcrepository/JdbcMedicinalHerbRepository.java
@@ -6,13 +6,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.rsreu.kibamba.lw1.models.MedicinalHerb;
 import ru.rsreu.kibamba.lw1.repositorys.MedicinalHerbRepository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -20,8 +23,10 @@ public class JdbcMedicinalHerbRepository implements MedicinalHerbRepository {
 
 
     private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
     public JdbcMedicinalHerbRepository(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(this.jdbcTemplate).withTableName("medicinal_herb").usingGeneratedKeyColumns("herb_id");
     }
     @Override
     public Iterable<MedicinalHerb> findAll() {
@@ -35,11 +40,15 @@ public class JdbcMedicinalHerbRepository implements MedicinalHerbRepository {
 
     @Override
     public MedicinalHerb save(MedicinalHerb medicinalHerb) {
-        jdbcTemplate.update(
-                "INSERT INTO MEDICINAL_HERB (herb_name, genus_type, family, distribution, piece, price, picture_link) VALUES" +
-                        "(?,?,?,?,?,?,?)",medicinalHerb.getName(),medicinalHerb.getGenusType(),medicinalHerb.getFamily(),
-                medicinalHerb.getDistribution(),medicinalHerb.getPiece(),medicinalHerb.getPrice(),""
-        );
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("herb_name",medicinalHerb.getName());
+        parameters.put("genus_type",medicinalHerb.getGenusType());
+        parameters.put("family",medicinalHerb.getFamily());
+        parameters.put("distribution",medicinalHerb.getDistribution());
+        parameters.put("piece",medicinalHerb.getPiece());
+        parameters.put("price",medicinalHerb.getPrice());
+        parameters.put("picture_link","");
+        simpleJdbcInsert.execute(parameters);
         return medicinalHerb;
     }
     private MedicinalHerb  mapRowToMedicinalHerb(ResultSet row, int rowNum) throws SQLException {
